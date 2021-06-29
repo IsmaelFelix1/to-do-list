c658bb4680c115f560e469e03d692912370fb479
Ismael Felix <ismael.duarte017@gmail.com>
2021-06-29 19:24:38 -0300

    loading user model

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
-const {Router} = require('express');
+import{Router} from'express';
+import User from'./app/models/User';
 
 const routes = new Router();
 
-routes.get('/teste', (req, res) => {
-    return res.json({ok: true});
+routes.get('/teste', async (req, res) => {
+    const user = await User.create({
+        name: 'Ismael',
+        email: 'ismael@gmail.com',
+        password_hash: '123123'
+    });
+    return res.json(user);
 })
 
-module.exports = routes;
+export default routes;
