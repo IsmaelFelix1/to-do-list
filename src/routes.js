9d2d834be92627b960d80f07ef8fb5a802e22523
Ismael Felix <ismael.duarte017@gmail.com>
2021-06-29 22:30:01 -0300

    authentication middleware

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import{Router} from'express';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.createUser);
-
+routes.put('/users',authMiddleware, UserController.updateUser);
 routes.post('/sessions', SessionController.session);
 
 export default routes;
