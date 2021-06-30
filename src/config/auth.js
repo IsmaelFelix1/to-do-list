fd346e5e9e5ea5b7f75acc63128e87df57bd5c26
Ismael Felix <ismael.duarte017@gmail.com>
2021-06-29 22:03:19 -0300

    jwt

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: '56437ee14d804bfa14762e0b1782827e',
+    expiresIn: '7d',
+}
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import{Router} from'express';
+import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.createUser);
+
+routes.post('/sessions', SessionController.session);
 
 export default routes;
